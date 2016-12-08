9a119fac3ae1dd6f5fd4a41a10cf31d51ce8894f
Oleg G.Kapranov <oleg@vertiopia.com>
2016-12-08 12:32:27 +0200

    fixted list-rentals-test

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -80,3 +80,33 @@ test('should show contents for a rentals', function (assert) {
     assert.equal(find('article .detail.bedrooms span').is(':visible'), true);
   });
 });
+
+test('should list available rentals.', function (assert) {
+  visit('/rentals');
+
+  andThen(function() {
+    assert.equal(find('.listing').length, 3, 'should see 3 listings');
+  });
+});
+
+// test('should filter the list of rentals by city.', function (assert) {
+//   visit('/rentals');
+//   fillIn('.list-filter input', 'seattle');
+//   keyEvent('.list-filter input', 'keyup', 69);
+//
+//   andThen(function() {
+//     assert.equal(find('.listing').length, 1, 'should show 1 listing');
+//     assert.equal(find('.listing .location:contains("Seattle")').length, 1, 'should contain 1 listing with location Seattle');
+//   });
+// });
+
+// test('should show details for a specific rental', function (assert) {
+//   visit('/rentals');
+//   click('a:contains("Grand Old Mansion")');
+//
+//   andThen(function() {
+//     assert.equal(currentURL(), '/rentals/grand-old-mansion', 'should navigate to show route');
+//     assert.equal(find('.show-listing h2').text(), "Grand Old Mansion", 'should list rental title');
+//     assert.equal(find('.description').length, 1, 'should list a description of the property');
+//   });
+// });
