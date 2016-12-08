454328311f8b122730380e9dedbf80e160d9464c
Oleg G.Kapranov <oleg@vertiopia.com>
2016-12-08 13:05:23 +0200

    added image to component

--- tests/integration/components/rental-listing-test.js
+++ tests/integration/components/rental-listing-test.js
@@ -1,25 +1,41 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
 moduleForComponent('rental-listing', 'Integration | Component | rental listing', {
   integration: true
 });
 
-test('it renders', function(assert) {
+// test('it renders', function(assert) {
+//   this.render(hbs`{{rental-listing}}`);
+//   assert.equal(this.$().text().trim(), '');
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+//   this.render(hbs`
+//     {{#rental-listing}}
+//       template block text
+//     {{/rental-listing}}
+//   `);
 
-  this.render(hbs`{{rental-listing}}`);
+//   assert.equal(this.$().text().trim(), 'template block text');
+// });
 
-  assert.equal(this.$().text().trim(), '');
+test('should toggle wide class on click', function(assert) {
+  assert.expect(3);
 
-  // Template block usage:
-  this.render(hbs`
-    {{#rental-listing}}
-      template block text
-    {{/rental-listing}}
-  `);
+  let stubRental = Ember.Object.create({
+    image: 'fake.png',
+    title: 'test-title',
+    owner: 'test-owner',
+    type: 'test-type',
+    city: 'test-city',
+    bedrooms: 3
+  });
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  this.set('rentalObj', stubRental);
+  this.render(hbs`{{rental-listing rental=rentalObj}}`);
+  assert.equal(this.$('.image.wide').length, 0, 'initially rendered small');
+  this.$('.image').click();
+  assert.equal(this.$('.image.wide').length, 1, 'rendered wide after click');
+  this.$('.image').click();
+  assert.equal(this.$('.image.wide').length, 0, 'rendered small after second click');
 });
