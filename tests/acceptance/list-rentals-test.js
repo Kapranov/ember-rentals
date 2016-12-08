3d80c3fde93f895ada9a37e26aa5a359a9050131
Oleg G.Kapranov <oleg@vertiopia.com>
2016-12-08 16:03:30 +0200

    added StubMapsService to list-rentals-test

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,7 +1,18 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'ember-rentals/tests/helpers/module-for-acceptance';
 
-moduleForAcceptance('Acceptance | list rentals');
+let StubMapsService = Ember.Service.extend({
+  getMapElement() {
+    return document.createElement('div');
+  }
+});
+
+moduleForAcceptance('Acceptance | list rentals', {
+  beforeEach() {
+    this.application.register('service:stubMaps', StubMapsService);
+    this.application.inject('component:location-map', 'maps', 'service:stubMaps');
+  }
+});
 
 test('visiting /rentals', function (assert) {
   visit('/rentals');
