7c6fb62c5b0a58f2471e1574d96a729dd36d5b15
Oleg G.Kapranov <oleg@vertiopia.com>
2016-12-09 13:04:41 +0200

    added liquid-fire

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -1,7 +1,4 @@
 import Ember from 'ember';
-import { animate } from 'liquid-fire';
-
-const duration = 400;
 
 export default Ember.Component.extend({
   classNames: ['list-filter'],
@@ -20,20 +17,3 @@ export default Ember.Component.extend({
     }
   }
 });
-
-export function flip() {
-  let promises = [];
-  if (this.oldElement) {
-    promises.push(animate(this.oldElement, {
-      rotateY: 179,
-      height: this.newElement ? this.newElement.height() : this.oldElement.height()
-    }, { duration }));
-  }
-  if (this.newElement) {
-    promises.push(animate(this.newElement, {
-      rotateY: [0, -179],
-      height: [this.newElement.height(), this.oldElement ? this.oldElement.height() : this.newElement.height()]
-    }, { duration }));
-  }
-  return Ember.RSVP.all(promises);
-}
